0cfa71132030f7b28aedc95c465db1f638252e61
DogeisCut <23178844+DogeisCut@users.noreply.github.com>
2026-05-22 02:34:31 -0600

    simple gravity, particle types, and safer grid

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use rand::Rng;
+
+enum ElementKind {
+    Powder,
+    Liquid,
+    Gas,
+    Solid
+}
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 enum Element {
@@ -13,6 +21,13 @@ enum Element {
             Element::Wood => 0.75,
         }
     }
+    pub fn kind(&self) -> ElementKind {
+        match self {
+            Element::Sand => ElementKind::Powder,
+            Element::Water => ElementKind::Liquid,
+            Element::Wood => ElementKind::Solid,
+        }
+    }
 }
 
 #[derive(Clone, Copy, Debug)]
@@ -22,6 +37,20 @@ struct Particle {
     vx: f32,
     vy: f32,
     element_type: Element,
+} impl Particle {
+    pub fn get_grid_x(&self) -> usize {
+        (self.x + 0.5) as usize
+    }
+    pub fn get_grid_y(&self) -> usize {
+        (self.y + 0.5) as usize
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+enum CellState {
+    Empty,
+    Occupied(usize),
+    OutOfBounds
 }
 
 struct IndexGrid {
@@ -37,8 +66,15 @@ struct IndexGrid {
         }
     }
 
-    pub fn get(&self, x: usize, y: usize) -> Option<usize> {
-        self.cells[x + (y * self.width)]
+    pub fn get(&self, x: isize, y: isize) -> CellState {
+        if x < 0 || y < 0 || x >= self.width as isize || y >= self.height as isize {
+            return CellState::OutOfBounds;
+        }
+
+        match self.cells[x as usize + (y as usize * self.width)] {
+            Some(index) => CellState::Occupied(index),
+            None => CellState::Empty,
+        }
     }
 
     pub fn set(&mut self, x: usize, y: usize, value: Option<usize>) {
@@ -67,15 +103,43 @@ struct World {
         for index in 0..self.particles_flat.len() {
             let particle = self.particles_flat[index];
             
-            let grid_x = (particle.x + 0.5) as usize;
-            let grid_y = (particle.y + 0.5) as usize;
+            let grid_x = particle.get_grid_x();
+            let grid_y = particle.get_grid_y();
             
             self.particles_grid.set(grid_x, grid_y, Some(index));
         }
     }
 
     pub fn update_physics(&mut self) {
+        for index in 0..self.particles_flat.len() {
+            let mut particle = self.particles_flat[index];
+
+            let grid_x: isize = particle.get_grid_x() as isize;
+            let grid_y: isize = particle.get_grid_y() as isize;
+
+
+            let picked: isize = if rand::random::<bool>() { 1 } else { -1 };
         
+            if self.particles_grid.get(grid_x, grid_y + 1) == CellState::Empty {
+                particle.y += 1.0;
+            } else {
+                if self.particles_grid.get(grid_x + picked, grid_y + 1) == CellState::Empty {
+                    particle.x += picked as f32;
+                    particle.y += 1.0;
+                } else if self.particles_grid.get(grid_x - picked, grid_y + 1) == CellState::Empty {
+                    particle.x -= picked as f32;
+                    particle.y += 1.0;
+                }
+            }
+            
+            self.particles_flat[index] = particle;
+            self.particles_grid.set(grid_x as usize, grid_y as usize, None);
+
+            let grid_x = particle.get_grid_x();
+            let grid_y = particle.get_grid_y();
+            
+            self.particles_grid.set(grid_x, grid_y, Some(index));
+        } 
     }
 }
 
